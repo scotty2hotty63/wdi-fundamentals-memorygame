af8afae26ceb32b3321468fd143484ca7f0259eb
Scott <ssolmonson63@gmail.com>
2019-11-28 10:53:00 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,11 @@
+console.log("Up and running"); //Test js is working
+
+//Cards for memory game
+var cardOne = "King";
+var cardTwo = "Queen";
+var cardThree = "Queen";
+var cardFour = "King";
+
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
