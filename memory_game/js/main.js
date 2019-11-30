470512b867439b02761f856e558220b998b15a66
Scott <ssolmonson63@gmail.com>
2019-11-30 08:46:46 -0500

    updated array to objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,27 +1,55 @@
 //Cards in game
-const cards = ["Queen", "Queen", "King", "King"];
+const cards = [
+	{
+		rank: "Queen",
+		suit: "Hearts",
+		cardImage: "images/queen-of-hearts.png",
+	},
+	{
+		rank: "Queen",
+		suit: "Diamonds",
+		cardImage: "images/queen-of-diamonds.png",
+	},
+	{
+		rank: "King",
+		suit: "Hearts",
+		cardImage: "images/king-of-hearts.png",
+	},
+	{
+		rank: "King",
+		suit: "Diamonds",
+		cardImage: "images/king-of-diamonds.png",
+	}
+];
 //Cards in play, empty at start
 const cardsInPlay = [];
 
 function checkForMatch() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+		//console.log("You found a match!");
+		alert("You found a match!");
+	} else if (cardsInPlay.length === 2) {
+		//console.log("Sorry try again.");
+		alert("Sorry try again.");
 	} else {
-		console.log("Sorry try again.");
+
 	}
-}
+};
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
-	//Check to see if 2 cards have been played AND match
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+		//Check to see if 2 cards have been played AND match
 	/*if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!")
 	} else {
 		alert("Sorry try again.")
 	}*/
-}
+};
+
 
 flipCard(0);
 flipCard(2);
