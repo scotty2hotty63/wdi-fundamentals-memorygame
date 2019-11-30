68cba5083e8d10b89ab305787fef021d6403bf14
Scott <ssolmonson63@gmail.com>
2019-11-30 04:47:33 -0500

    created improved game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,22 @@
-console.log("Up and running"); //Test js is working
-
-//Cards for memory game
-var cardOne = "King";
-var cardTwo = "Queen";
-var cardThree = "Queen";
-var cardFour = "King";
-
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+//Cards in game
+const cards = ["Queen", "Queen", "King", "King"];
+//Cards in play, empty at start
+const cardsInPlay = [];
+//First card in array
+const cardOne = cards[0];
+//Add card to in play
+cardsInPlay.push(cardOne);
+//Test work, display in console
+console.log("User flipped" + " " + cardsInPlay[0]);
+//Second card flipped
+const cardTwo = cards[1];
+//Add second card to in play
+cardsInPlay.push(cardTwo);
+//Test message in console
+console.log("User flipped" + " " + cardsInPlay[1]);
+//Check to see if 2 cards have been played
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!")
+} else {
+	alert("Sorry try again.")
+}
