077a9e05646d7f1c5675db5359901f30d422207e
Scott <ssolmonson63@gmail.com>
2019-11-30 07:03:11 -0500

    added functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,21 +2,26 @@
 const cards = ["Queen", "Queen", "King", "King"];
 //Cards in play, empty at start
 const cardsInPlay = [];
-//First card in array
-const cardOne = cards[0];
-//Add card to in play
-cardsInPlay.push(cardOne);
-//Test work, display in console
-console.log("User flipped" + " " + cardsInPlay[0]);
-//Second card flipped
-const cardTwo = cards[1];
-//Add second card to in play
-cardsInPlay.push(cardTwo);
-//Test message in console
-console.log("User flipped" + " " + cardsInPlay[1]);
-//Check to see if 2 cards have been played
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!")
-} else {
-	alert("Sorry try again.")
+
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry try again.");
+	}
 }
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+	//Check to see if 2 cards have been played AND match
+	/*if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!")
+	} else {
+		alert("Sorry try again.")
+	}*/
+}
+
+flipCard(0);
+flipCard(2);
